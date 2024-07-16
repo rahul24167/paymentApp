73c80dc1e3d5f78a68c956d05ea5bb5005e71b6d
rahul24167 <113711057+rahul24167@users.noreply.github.com>
2024-07-16 22:14:39 +0530

    added Route of landing page in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
+import {LandingPage} from "./pages/LandingPage.jsx";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
@@ -13,6 +14,7 @@ function App() {
     <>
        <BrowserRouter>
         <Routes>
+          <Route path="/" element={<LandingPage/>}></Route>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
